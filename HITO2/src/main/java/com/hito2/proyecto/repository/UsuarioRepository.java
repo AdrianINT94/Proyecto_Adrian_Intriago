ef9e0b263470739668718073e1e5922672e35778
AdrianINT94 <wkj62135@educastur.es>
2026-01-14 12:16:49 +0100

    Delete HITO2 directory

--- HITO2/src/main/java/com/hito2/proyecto/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hito2.proyecto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hito2.proyecto.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-}
